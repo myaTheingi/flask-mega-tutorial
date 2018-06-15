1733942738e4b39f7d71a322e86aa8c42c9c4bb1
myaTheingi <myaTheingi>
2018-06-15 15:02:33 +0530

    add flask-test1.py

---
+++ templates/flask-test1.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return """
+    <html>
+    <head><title>This is my flask app</title></head>
+    <body><h1>Hello from Flask</h1></body>
+    </html>
+    """
+    
+if __name__ == "__main__":
+    app.run(debug=True)
